c01b0b56053cfce0b183597458b79481088e32ad
Holger <holger.trittenbach@gmail.com>
2014-05-14 09:22:14 +0200

    changed setup for simulation run to set back all parameters refactoring, method extraction

--- SimulatedAnnealingExample/src/de/kit/nio/SimulatedAnnealing.java
+++ SimulatedAnnealingExample/src/de/kit/nio/SimulatedAnnealing.java
@@ -8,7 +8,52 @@ public class SimulatedAnnealing {
 	}
 
 	public static void main(String[] args) {
+		
+		TestFunction testFunction = setupFunctions();
+		
+		System.out.println("n\tx-cor\ty-cor\tf(x,y)\tt-max");
+		
+		for (int n = 1; n <= 10; n++) {
+			startSimulationRuns(testFunction, n);
+		}
+	}
 
+	private static void startSimulationRuns(TestFunction testFunction, int n) {
+		int t = 0;
+		Solution currentSolution;
+		currentSolution = new Solution(-5.12 + Math.random()
+				* (5.12 * 2), -5.12 + Math.random() * (5.12 * 2));
+		double currentEnergy;
+		currentEnergy = calculateFunctionValue(testFunction,
+				currentSolution);
+		
+		Solution bestSolution = currentSolution;
+		double bestEnergy = currentEnergy;
+		
+		double T = 180;
+		double alpha = 0.9;
+		double distance = 0.1;
+		
+		runSample(testFunction, n, t, currentSolution, currentEnergy,
+				bestSolution, bestEnergy, T, alpha, distance);
+	}
+
+	private static void runSample(TestFunction testFunction, int n, int t,
+			Solution currentSolution, double currentEnergy,
+			Solution bestSolution, double bestEnergy, double T, double alpha,
+			double distance) {
+		for (int maxt = 10; maxt <= 1000000; maxt = maxt * 10) {
+			bestSolution = runSimulatedAnnealing(testFunction, t,
+					currentSolution, currentEnergy, bestSolution,
+					bestEnergy, T, alpha, distance, maxt);
+			System.out.printf("%d\t%f\t%f\t%f\t%d \n", n,bestSolution.getX(),
+					bestSolution.getY(),
+					calculateFunctionValue(testFunction, bestSolution),
+					maxt);
+		}
+	}
+
+	private static TestFunction setupFunctions() {
 		TestFunction deJong = (x, y) -> Math.pow(x, 2) + Math.pow(y, 2);
 		TestFunction hyperEllepsoid = (x, y) -> Math.pow(x, 2) + 2
 				* Math.pow(y, 2);
@@ -19,64 +64,39 @@ public class SimulatedAnnealing {
 		TestFunction rastrigrin = (x, y) -> 20
 				+ (Math.pow(x, 2) - 10 * Math.cos(2 * Math.PI * x))
 				+ (Math.pow(y, 2) - 10 * Math.cos(2 * Math.PI * y));
+		TestFunction testFunction = deJong;
+		return testFunction;
+	}
 
-		TestFunction testFunction = rastrigrin;
-
-		Solution currentSolution = new Solution(-5.12 + Math.random()
-				* (5.12 * 2), -5.12 + Math.random() * (5.12 * 2));
-		double currentEnergy = calculateFunctionValue(testFunction,
-				currentSolution);
-
+	private static Solution runSimulatedAnnealing(TestFunction testFunction,
+			int t, Solution currentSolution, double currentEnergy,
+			Solution bestSolution, double bestEnergy, double T, double alpha,
+			double distance, int maxt) {
 		Solution newSolution;
 		double newEnergy;
-
-		Solution bestSolution = currentSolution;
-		double bestEnergy = currentEnergy;
- 
-		double T = 180;
-		double alpha = 0.9;
-		double distance = 0.1;
-		System.out.println("n\tx-cor\ty-cor\tf(x,y)\tt-max");
-		for (int n = 1; n <= 10; n++) {
-			int t = 0;
-			for (int maxt = 10; maxt <= 1000000; maxt = maxt * 10) {
-				while (t < maxt) {
-					newSolution = currentSolution.getRandomNeighbor(distance);
-					newEnergy = calculateFunctionValue(testFunction,
-							newSolution);
-
-					if (newEnergy <= currentEnergy) {
-						currentSolution = newSolution;
-						currentEnergy = newEnergy;
-					} else {
-						if (Math.random() < Math
-								.exp((currentEnergy - newEnergy)) / T) {
-							currentSolution = newSolution;
-							currentEnergy = newEnergy;
-						}
-					}
-
-					if (currentEnergy < bestEnergy) {
-						bestSolution = currentSolution;
-						bestEnergy = currentEnergy;
-					}
-
-					T = alpha * T;
-					t++;
-					// System.out.printf("Current solution x=%f y=%f f(x,y)=%f\n",
-					// currentSolution.getX(), currentSolution.getY(),
-					// currentEnergy);
+		while (t < maxt) {
+			newSolution = currentSolution.getRandomNeighbor(distance);
+			newEnergy = calculateFunctionValue(testFunction,
+					newSolution);
+			if (newEnergy <= currentEnergy) {
+				currentSolution = newSolution;
+				currentEnergy = newEnergy;
+			} else {
+				if (Math.random() < Math
+						.exp((currentEnergy - newEnergy)) / T) {
+					currentSolution = newSolution;
+					currentEnergy = newEnergy;
 				}
-				// System.out.printf("Best result x=%f y=%f f(x,y)=%f # number of iterations=%d \n",
-				// bestSolution.getX(), bestSolution.getY(),
-				// calculateFunctionValue(testFunction, bestSolution),
-				// maxt);
-				System.out.printf("%d\t%f\t%f\t%f\t%d \n", n,bestSolution.getX(),
-						bestSolution.getY(),
-						calculateFunctionValue(testFunction, bestSolution),
-						maxt);
 			}
+			if (currentEnergy < bestEnergy) {
+				bestSolution = currentSolution;
+				bestEnergy = currentEnergy;
+			}
+
+			T = alpha * T;
+			t++;
 		}
+		return bestSolution;
 	}
 
 }
